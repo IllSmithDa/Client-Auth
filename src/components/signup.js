c9236915f71044e1fc9d33441c39a44c3524336f
Sam Kim <sams404link@gmail.com>
2017-12-13 17:33:02 -0800

    sprint updated

--- src/components/signup.js
+++ src/components/signup.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import { register } from '../actions';
+import { combineReducers } from '../../../../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/redux';
 
 class SignUp extends Component {
   // This component needs a `handleFormSubmit` function that takes in
   // username, password, comfirmPassword strings as input and
   // invokes the `register` action
+  handleFormSubmit = ({ username, password, confirmPassword }) => {
+    this.props.register(username, password, confirmPassword, this.props.history);
+  }
 
   renderAlert = () => {
     if (!this.props.error) return null;
@@ -18,7 +22,25 @@ class SignUp extends Component {
     // Use reduxForm to build the sign up form
     // Check the other components to see how reduxForm is used
     // There needs fields for Username, Password, and Confirm Password
-    return <div>Sign Up</div>;
+    const { handleSubmit } = this.props;
+    return(
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+      <fieldset>
+        <label>Username:</label>
+        <Field name="username" component="input" type="text" />
+      </fieldset>
+      <fieldset>
+        <label>Password:</label>
+          <Field name="password" component="input" type="password" />
+      </fieldset>
+      <fieldset>
+        <label>Confirm Password:</label>
+        <Field name="confirmPassword" component="input" type="password" />
+      </fieldset>
+      <button action="submit">Sign Up</button>
+      {this.renderAlert()}
+      </form>
+    );
   }
 }
 
@@ -29,7 +51,7 @@ const mapStateToProps = state => {
 };
 
 // Make sure to correctly fill in this `connect` call
-SignUp = connect(null)(SignUp);
+SignUp = connect(mapStateToProps, { register })(SignUp);
 
 export default reduxForm({
   form: 'signup',
